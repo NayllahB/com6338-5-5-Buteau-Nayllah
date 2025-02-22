e01d3820f7ba124d90c749458b72f6d47eb5fac1
Nayllah Buteau <nayllah.buteau@ufl.edu>
2025-02-22 16:39:58 -0500

    completed js

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var form = document.getElementById('add-todo')
+
+form.onsubmit = function(e) {
+    e.preventDefault()
+    //grab inputed item
+    var formField = document.querySelector('input')
+    var data = formField.value.trim()
+
+    // does not add todo if field is empty
+    if (data === ''){
+        return
+    }
+    
+    // display todo below form
+    var ulElement = document.getElementById('todo-list')
+    var liElement = document.createElement('li')
+    ulElement.appendChild(liElement)
+    var listButton = document.createElement('button')
+    liElement.appendChild(listButton)
+    listButton.textContent = data
+
+    //clears form field after input
+    formField.value = ""
+
+    var clickCount = 0
+
+    // strikes through list item when clicked
+    listButton.addEventListener('click', function(e) {
+        listButton.style.textDecoration = 'line-through'
+        clickCount ++
+
+        if(clickCount === 2){
+            liElement.remove()
+        }
+    })
+
+}
+
